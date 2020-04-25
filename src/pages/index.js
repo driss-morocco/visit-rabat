55e995800bf2a9e6d713903d342e1a6ebbc1ffe4
sameep <sameepk@yahoo.com>
2020-04-25 09:56:55 +0300

    update-index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,33 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Things To Do in Rabat" />
+    <h1>Things To Do in Rabat</h1>
+   <p>Rabat is the capital of Morocco and one of the country's four imperial cities. However, it seems that many travellers pass by it because they don't know what to see in Rabat. Contrary to what it may seem, <a href="https://thinkmorocco.com/things-to-do-in-rabat/">Rabat is a quiet, modern capital</a>, but it still keeps tradition and has corners that will surprise you.</p>
+<p><img style="display: block; margin-left: auto; margin-right: auto;" src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Tour_Hassan-Rabat.jpg" alt="things to do in rabat" width="800" height="533" /></p>
+<p>Unlike its neighbour Casablanca, Rabat has a more relaxed coastal feel than its neighbour. In addition, the traffic is much more bearable. What there is to see in Rabat may not be as prominent a priori as in other cities in the country, such as Marrakech or Fez. But that may be where the charm of this capital city lies, and it doesn't mind that sometimes it is not taken as such. Rabat has enough places to visit for at least one day. The advantage is that you can discover the city at your own pace and without rushing.</p>
+<p>If you are also wondering what to see in Rabat, we will tell you about some of the places where you can discover the history of this city, as well as other places you cannot miss or places to stay.</p>
+<p>What to see in Rabat</p>
+<p><br /><strong>Historical places in Rabat</strong></p>
+<ul>
+<li>Tower of Hasan</li>
+<li>The Mausoleum of Mohamed V</li>
+<li>Necropolis of Chellah</li>
+<li>The Medina of Rabat</li>
+<li>The Kasbah of the Udayas</li>
+</ul>
+<p><br /><strong>Leaving the historic centre</strong></p>
+<ul>
+<li>Ville Nouvelle or New Town</li>
+<li>Walk along the coast</li>
+</ul>
+<p><br /><strong>Hotels in Rabat</strong></p>
+<ul>
+<li>For loose pockets: more than 100 euros per night</li>
+<li>For average budgets: between 50 and 100</li>
+<li>For all pockets</li>
+<li>More hotels in Rabat: less than 50</li>
+</ul>
   </Layout>
 )
 
